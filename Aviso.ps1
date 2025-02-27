602ce09584bbb946bfd67dc3f6f0793c5578f195
Atuncillo <135758590+Atuncillo@users.noreply.github.com>
2025-02-27 11:13:54 -0300

    Update and rename Aviso.ps1 to test.bat

--- Aviso.ps1
+++
@@ -1,2 +0,0 @@
-Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show('Guarda todos tus archivos y documentos, porque en 30 minutos ocurrirá un reinicio forzoso.', 'Aviso', 'OK', 'Warning')
