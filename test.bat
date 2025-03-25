63e90c8b5a5c87e959d5a927f14f04586abb8f4d
Atuncillo <135758590+Atuncillo@users.noreply.github.com>
2025-03-25 11:30:51 -0300

    Delete test.bat

--- test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Guarda todos tus archivos y documentos, porque en 30 minutos ocurrirá un reinicio forzoso.', 'Aviso', 'OK', 'Warning')"
-exit
